d8129f295926217cf6d67530261a1eed53c7806a
unknown <juan.cortes-p@labinfo.is.escuelaing.edu.co>
2022-08-24 12:33:26 -0500

    httpconnectionexample

---
+++ src/main/java/edu/escuelaing/arem/MDV.java
@@ -0,0 +1,26 @@
+package edu.escuelaing.arem;
+
+import edu.escuelaing.arem.httpconnection.HttpConnectionExample;
+
+import static spark.Spark.*;
+
+public class MDV{
+
+    public static void main(String[] args) {
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+
+        get("/intraday",(req,res)  -> {
+            res.type("application/json");
+            return HttpConnectionExample.getAPI();
+        });
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            System.out.println(System.getenv("PORT"));
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
+    }
+}
